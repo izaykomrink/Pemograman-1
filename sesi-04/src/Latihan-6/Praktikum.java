9a9779b71f57c22465b40aa289dc58508e9571a2
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:32:41 +0700

    praktikum

---
+++ sesi-04/src/Latihan-6/Praktikum.java
@@ -0,0 +1,12 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String args[]){
+		boolean coba1;
+		
+		coba1 = (10 == 10);
+			System.out.println(coba1);
+		coba1 = (10 != 10);
+			System.out.println(coba1);
+	}
+}
