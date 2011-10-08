7134abf0ea2df6837702429495cf0d2e3d5b7b87
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:34:13 +0700

    praktikum

---
+++ sesi-04/src/Latihan-8/Praktikum.java
@@ -0,0 +1,14 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String args[]){
+		//dideklarasikan variabel count = 1 dengan tipe data integer.
+		int count = 1;
+		//variabel dengan tipe data count < 5.
+		while(count < 5){
+			//untuk menjalankan variabel dengan cara memanggil dan menampilkan data dari variabel lain.
+			System.out.println("Count is aqual to " + count);
+			count++;
+		}
+	}
+}
