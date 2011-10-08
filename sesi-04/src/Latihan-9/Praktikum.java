b5fcf2260c23d34ff5a7de9effc7419dc46b3c5a
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:34:42 +0700

    praktikum

---
+++ sesi-04/src/Latihan-9/Praktikum.java
@@ -0,0 +1,25 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String[] args){
+		//dideklarasikan variabel a=4, b=6 dan num=a/b dengan tipe data integer.
+		int a=4,b=6;
+		int num=a|b;
+		//menampilkan variabel (num==4) dan akan menampilkan "value is 4"
+		if(num==4)
+			//variabel yang akan ditampilkan dari (num==4)
+			System.out.println("value is 4");
+		//menampilkan variabel tidak sama dengan (num==5) dan akan menampilkan "value is 5"
+		else if (num==5)
+			//variabel yang akan ditampilkan dari (num==5)
+			System.out.println("value is 5");
+		//menampilkan variabel tidak sama dengan (num==6) dan akan menampilkan "value is 6"
+		else if(num==6)
+			//variabel yang akan ditampilkan dari (num==6)
+			System.out.println("value is 6");
+		//menampilkan variabel tidak sama dengan (num==7) dan akan menampilkan "value is 7"
+		else if(num==7)
+			//variabel yang akan ditampilkan dari (num==7)
+			System.out.println("value is 7");
+	}
+}
