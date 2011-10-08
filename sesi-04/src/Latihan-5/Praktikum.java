ed5d57aadd8bd02653ca071e541966c41d487029
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:32:10 +0700

    praktikum

---
+++ sesi-04/src/Latihan-5/Praktikum.java
@@ -0,0 +1,12 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String args[]){
+	//dideklarasikan variabel test = 5 dengan tipe data integer.
+		int test = 5;
+		//untuk menampilkan data dari variabel test = 5 dengan nilai yang sama.
+		System.out.println(test++);
+		//untuk menampilkan data dari variabel test = 5 dengan nilai yang berbeda.
+		System.out.println(test);
+	}
+}
