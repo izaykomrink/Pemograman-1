b3297b4c916b1d8e4d31176c1b620b9c10067db4
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:31:43 +0700

    praktikum

---
+++ sesi-04/src/Latihan-4/Praktikum.java
@@ -0,0 +1,19 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String[] args){
+		//dideklarasikan variabel 3>2 dan variabel else dengan tipe data if.
+		System.out.println("Instr 1");
+		//variabel perbandingan jika 3>2 maka data akan ditampilkan variabel tidak akan menampilkan data yang sama.
+		if(3>2){
+			//menampilkan variabel dari 3>2
+			System.out.println("Instr 2");
+		//membandingkan variabel apabila 3>2 tidak maka data tidak akan ditampilkan.
+		}else{
+			//menampilkan variabel dari 3>2 dengan tipe data else.
+			System.out.println("Instr 3");
+		}
+		//menampilkan variabel dari 3>2
+		System.out.println("Instr 4");
+	}
+}
