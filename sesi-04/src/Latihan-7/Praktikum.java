ad7b8cb8797e5dd21e8e72864e98b1a1ae3ef724
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:33:38 +0700

    praktikum

---
+++ sesi-04/src/Latihan-7/Praktikum.java
@@ -0,0 +1,10 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String[] args){
+		//dideklarasikan variabel age = 21 dengan tipe data integer.
+		int age = 21;
+			//menampilkan variabel age > 50. apabila nilai 21 > 50 maka maka salah, apabila sebaliknya maka benar.
+			System.out.println(age > 50 ? "You are ald" : "You are young");
+	}
+}
