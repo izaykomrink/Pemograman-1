aeba91157d82225d5174f781597c2236840186fe
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:35:04 +0700

    praktikum

---
+++ sesi-04/src/Latihan-10/Belajar.java
@@ -0,0 +1,15 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Belajar{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String[] args){
+		//dideklarasikan variabel num=11 dan num >5 dengan tipe data integer.
+		int num=11;
+		if(num > 5)
+			//akan menampilkan nilai dari 11>5 maka benar dan akan ditampilkan.
+			System.out.println("Number is Greater Than 5");
+		//jika num lebih kecil maka salah
+		else
+			//variabel akan ditampilkan number is less Than 5
+			System.out.println("Number is less Than 5");
+	}
+}
