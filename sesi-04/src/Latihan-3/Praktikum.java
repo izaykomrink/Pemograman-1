0d7f52bebedc5d3bb92a729a9f06bf46357cdb21
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:31:12 +0700

    praktikum

---
+++ sesi-04/src/Latihan-3/Praktikum.java
@@ -0,0 +1,11 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String args[]){
+		//dideklarasikan variabel dengan tipe data while (true) untuk menjalankan perintah tanpa batas
+		while(true){
+			//perintah untuk menampilkan variabel tanpa batas.
+			System.out.println("Hello, Worlds we are using loops");
+		}
+	}
+}
