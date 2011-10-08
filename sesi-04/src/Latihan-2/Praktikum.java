91c223a6eab881c513bc95ca334bfb2caeef2672
izay komrink <izay_tea@yahoo.com>
2011-10-08 10:30:21 +0700

    praktikum

---
+++ sesi-04/src/Latihan-2/Praktikum.java
@@ -0,0 +1,11 @@
+//class ini untuk menampilkan nama pada filenya.
+public class Praktikum{
+	//method utama yang akan menjalankan oleh java.
+	public static void main(String args[]){
+		//dideklarasikan variabel x dengan tipe data for (integer x) akan menghitung variabel dari 0-6
+		for (int x = 0; x < 7; x++){
+			//menjalankan perintah dengan variabel perbandingan x < 7 apabila x lebih kecil dari 7 maka perintah dijalankan.
+			System.out.println("Hello, Worlds we are using loops");
+		}
+	}
+}
