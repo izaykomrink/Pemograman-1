78ec06cf353b632b40b3c6a5efe77aa99ea01192
izay komrink <izay_tea@yahoo.com>
2011-10-12 14:50:39 +0700

    Laptop

---
+++ sesi-05/src/tugas1/Laptop.java
@@ -0,0 +1,29 @@
+public class Laptop{
+	private String merk;
+	private String processor;
+	private String harga;
+	
+	public String getHarga(){
+		return harga;
+	}
+	
+	public void setHarga(String harga){
+		this.harga = harga;
+	}
+	
+	public String getProcessor(){
+		return processor;
+	}
+	
+	public void setProcessor (String harga){
+		this.processor = processor;
+	}
+	
+	public String getMerk(){
+		return merk;
+	}
+	
+	public void setMerk (String merk){
+		this.merk = merk;
+	}
+}
