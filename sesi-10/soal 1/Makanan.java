195becdef7ef42a976c528cad4f847b24129230c
izay komrink <izay_tea@yahoo.com>
2011-11-23 17:13:20 +0700

    sesi10

---
+++ sesi-10/soal 1/Makanan.java
@@ -0,0 +1,5 @@
+public class Makanan{
+	void santap(){
+		System.out.println("saya Minum Es Teh Manis");
+	}
+}
---
+++ sesi-10/soal 3/EkspresiWajah.java
@@ -0,0 +1,20 @@
+public class EkspresiWajah{
+	public String respons(){
+		return("Perhatikan ekspesi wajah saya");
+	}
+}
+class Gembira extends EkspresiWajah{
+	public String respons(){
+		return("ckckckckckkckkkckck...");	
+	}
+}
+class Sedih extends EkspresiWajah{
+	public String respons(){
+		return ("wkwkwkwkwkkwkwkwkwkkwkwk");
+	}
+}
+class Marah extends EkspresiWajah{
+	public String respons(){
+		return ("Haaahhh goreng sia....");
+	}
+}
